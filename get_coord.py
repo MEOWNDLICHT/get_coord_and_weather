38707e7d353921cc55df6d6d7720e279da457968
meow <meowndlicht@gmail.com>
2025-07-31 00:26:49 +0800

    fixed and improved get_coord.py

--- get_coord.py
+++ get_coord.py
@@ -1,26 +1,31 @@
 # COORDINATES LOOKUP USING OPENWEATHERMAP'S API
-""" Gets the coordinates (latitude and longitude) of a place using an api. """
 
 import requests
 
 
 def get_coordinates(place: str) -> dict[str, float]: 
-    api_key = str(input('Enter your API KEY here (from openweathermap): '))
+    api_key = str(input('Enter your API KEY here to access the coordinates info (from openweathermap): '))
 
-    if not api_key:
-        print('Enter your API.')
-        return
 
     url = f"http://api.openweathermap.org/geo/1.0/direct?q={place}&appid={api_key}"
 
     try:
         response = requests.get(url)
+
+        # For checking the API key
+        if response.status_code == 401:
+            print('Enter a valid API key.')
+            return
+        
         data = response.json()
         coordinates = {"lat" : data[0]['lat'], "lon" : data[0]['lon']}
     
+
+
     except requests.exceptions.HTTPError as http_error:
         print(f"An HTTP error occured: {http_error}")
         return()
+    
     
     except IndexError:
         print(f"\n'{place}' cannot be found!\nEnter a valid place.\n")
@@ -29,7 +34,7 @@ def get_coordinates(place: str) -> dict[str, float]:
     except KeyError:
         print("\nPlease enter anything. As long as it's a real place.\n")
         return
-
+    
     return coordinates
 
 
@@ -37,5 +42,6 @@ def get_coordinates(place: str) -> dict[str, float]:
 if __name__ == '__main__':
     place = str(input("Enter a place -> "))
     coordinates_data = get_coordinates(place)
+
     print("\nThe coordinates of the place are:")
     print(f"lat: {coordinates_data['lat']}\nlon: {coordinates_data['lon']}\n")
