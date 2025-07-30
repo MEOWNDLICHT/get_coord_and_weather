56233331d457fdf7d0c9db3dd09182a93446c29e
meow <meowndlicht@gmail.com>
2025-07-31 01:03:58 +0800

    improved the error handling for get_weather.py

--- get_weather.py
+++ get_weather.py
@@ -6,25 +6,25 @@ import requests
 from get_coord import get_coordinates
 
 
-def get_current_weather(coords):
+def get_current_weather(coords: dict):
     if not coords:
         return
     
-    api_key = str(input('Enter your API KEY here (from weatherapi): '))
-
-    if not api_key:
-        print('Please enter your API.')
-        return
-
+    api_key = str(input('Enter your API KEY here to access the weather info (from weatherapi): '))
     url = f"http://api.weatherapi.com/v1/current.json?key={api_key}&q={coords['lat']},{coords['lon']}"
+    
+    
     response = requests.get(url)
-    data = response.json()
 
-
-    if response.status_code != 200:
+    if response.status_code == 401:
+        print('Enter a valid API key to access weather information.')
+        return
+    elif response.status_code != 200:
         print("\nThe site may be down, my apologies.\n")
         return
+        
 
+    data = response.json()
 
     if data['current']['is_day'] == 1:
         data['current']['is_day'] = "Day" 
@@ -55,14 +55,19 @@ if __name__ == "__main__":
     coordinates = get_coordinates(search_place)
     weather_result = get_current_weather(coordinates)
 
-    print(f"\nYou searched for: {search_place.capitalize()}")
 
-    print("\nGENERAL INFORMATION")
-    for category, info in weather_result['location_info'].items():
-        print(f"{category.capitalize()}: {info}")
+    while True:
+        if coordinates or weather_result:
+            print(f"\nYou searched for: {search_place.capitalize()}")
 
-    print("\nCURRENT WEATHER")
-    for condition, info in weather_result['weather_data'].items():
-        condition = condition.replace("_", " ").capitalize()
-        print(f"{condition.capitalize()}: {info}")
-    print()
+            print("\nGENERAL INFORMATION")
+            for category, info in weather_result['location_info'].items():
+                print(f"{category.capitalize()}: {info}")
+                
+
+            print("\nCURRENT WEATHER")
+            for condition, info in weather_result['weather_data'].items():
+                condition = condition.replace("_", " ").capitalize()
+                print(f"{condition.capitalize()}: {info}")
+            print()
+        break
