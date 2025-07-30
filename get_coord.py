01a2fe1e5c70e18aef395cf0a93aaecb529dcd56
meow <meowndlicht@gmail.com>
2025-07-30 17:29:13 +0800

    added the initial code for the project

---
+++ get_coord.py
@@ -0,0 +1,41 @@
+# COORDINATES LOOKUP USING OPENWEATHERMAP'S API
+""" Gets the coordinates (latitude and longitude) of a place using an api. """
+
+import requests
+
+
+def get_coordinates(place: str) -> dict[str, float]: 
+    api_key = str(input('Enter your API KEY here (from openweathermap): '))
+
+    if not api_key:
+        print('Enter your API.')
+        return
+
+    url = f"http://api.openweathermap.org/geo/1.0/direct?q={place}&appid={api_key}"
+
+    try:
+        response = requests.get(url)
+        data = response.json()
+        coordinates = {"lat" : data[0]['lat'], "lon" : data[0]['lon']}
+    
+    except requests.exceptions.HTTPError as http_error:
+        print(f"An HTTP error occured: {http_error}")
+        return()
+    
+    except IndexError:
+        print(f"\n'{place}' cannot be found!\nEnter a valid place.\n")
+        return
+    
+    except KeyError:
+        print("\nPlease enter anything. As long as it's a real place.\n")
+        return
+
+    return coordinates
+
+
+
+if __name__ == '__main__':
+    place = str(input("Enter a place -> "))
+    coordinates_data = get_coordinates(place)
+    print("\nThe coordinates of the place are:")
+    print(f"lat: {coordinates_data['lat']}\nlon: {coordinates_data['lon']}\n")
---
+++ get_weather.py
@@ -0,0 +1,68 @@
+# WEATHER LOOKUP PROGRAM USING AN API FROM WEATHERAPI.COM 
+""" Gets the current weather condition along with some general info about the entered location """
+
+
+import requests
+from get_coord import get_coordinates
+
+
+def get_current_weather(coords):
+    if not coords:
+        return
+    
+    api_key = str(input('Enter your API KEY here (from weatherapi): '))
+
+    if not api_key:
+        print('Please enter your API.')
+        return
+
+    url = f"http://api.weatherapi.com/v1/current.json?key={api_key}&q={coords['lat']},{coords['lon']}"
+    response = requests.get(url)
+    data = response.json()
+
+
+    if response.status_code != 200:
+        print("\nThe site may be down, my apologies.\n")
+        return
+
+
+    if data['current']['is_day'] == 1:
+        data['current']['is_day'] = "Day" 
+    else:
+        data['current']['is_day'] = "Night"
+
+
+    result = {
+        "location_info" : 
+            {"country" : data['location']['country'],
+            "place" : data['location']['name'],
+            "region" : data['location']['region'],
+            "localtime" : data['location']['localtime']},
+        
+        "weather_data" : 
+            {"current_temperature" : data['current']['temp_c'],
+            "feels_like" : data['current']['feelslike_c'],
+            "current_cycle" : data['current']['is_day'],
+            "current_condition" : data['current']['condition']['text']}
+            }
+    
+    return result
+
+
+
+if __name__ == "__main__":
+    search_place = str(input("Enter the name of the place -> "))
+    coordinates = get_coordinates(search_place)
+    weather_result = get_current_weather(coordinates)
+
+    print(f"\nYou searched for: {search_place.capitalize()}")
+
+    print("\nGENERAL INFORMATION")
+    for category, info in weather_result['location_info'].items():
+        print(f"{category.capitalize()}: {info}")
+
+    print("\nCURRENT WEATHER")
+    for condition, info in weather_result['weather_data'].items():
+        condition = condition.replace("_", " ").capitalize()
+        print(f"{condition.capitalize()}: {info}")
+    print()
